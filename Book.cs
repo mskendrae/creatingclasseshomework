3915cc044b5f24354ff97fd6811189c666edb787
Kendra Egert <Kendra Egert>
2016-09-16 06:22:50 -0400

    first commit

---
+++ Book.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace creatingclasseshomework
+{
+    class Book
+    {
+
+        //instance variables
+        private string title; 
+        private string author;
+        private int pages;
+        private int wordcount;
+
+        //contructor (has same name as class) method user providing books title
+        public Book(string title)
+              {
+                this.title = title;  //allow to access the instance variable (above)                            
+              }
+
+
+        // methods
+       
+            
+        public void SetTitle(string title)
+              {
+                 this.title = title;
+              }
+        public string GetTitle()
+              {
+                  return title;
+              }
+        public void AssignWordCountFromText(string text)
+             {
+               wordcount = text.Split(' ').Length;
+             }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace creatingclasseshomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //testing out the new class book created from reading homework
+            Book book = new Book("Harry Potter");
+
+            //change my ming lets do full title
+            book.SetTitle("Hrry Potter ns rhw Half Blood Prince");
+
+            //Now I for got what was the title again
+            Console.WriteLine(Book.GetTitle());
+
+
+
+
+        }
+    }
+}
